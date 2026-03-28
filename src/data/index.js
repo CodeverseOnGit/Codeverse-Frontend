524e0dd93a54ae06bf9044fd8ee395bc13aaf084
CodeverseOnGit <Codeverse1704@outlook.com>
2026-03-28 23:24:22 +0300

    added backend engineering module

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,6 @@
 import java from './java.json'
 import swe from './swe.json'
 import spring from './spring.json'
+import backend from './backend-engineering.json'
 
-export const sections = [java, swe, spring]
+export const sections = [java, swe, spring, backend]
